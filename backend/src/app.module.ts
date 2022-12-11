f484ea6c228f04c955f2f12225e29a35c0afb4dc
Vadim <vadim192000@gmail.com>
2022-12-11 20:22:10 +0300

    implemented actor module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,31 +1,32 @@
-import { FileModule } from './file/file.module';
-import { FileService } from './file/file.service';
-import { GenreModule } from './genre/genre.module';
-import { UserModule } from './user/user.module';
-import { UserService } from './user/user.service';
-import { AuthModule } from './auth/auth.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypegooseModule } from 'nestjs-typegoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GenreModule } from './genre/genre.module';
+import { ActorModule } from './actor/actor.module';
+import { AuthModule } from './auth/auth.module';
 import { getMongoDbConfig } from './config/mongo.config';
+import { FileModule } from './file/file.module';
+import { UserModule } from './user/user.module';
+
+import { TypegooseModule } from 'nestjs-typegoose';
 
 @Module({
   imports: [
-    FileModule,
-    GenreModule,
-    UserModule,
-    AuthModule,
     ConfigModule.forRoot(),
     TypegooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: getMongoDbConfig,
     }),
+    GenreModule,
+    ActorModule,
+    UserModule,
     AuthModule,
+    FileModule,
   ],
   controllers: [AppController],
-  providers: [FileService, AppService],
+  providers: [AppService],
 })
 export class AppModule {}
