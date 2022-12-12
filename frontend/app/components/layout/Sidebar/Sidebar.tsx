5a4ae651fa0ebf6c168f9cceb6bb12075fb9965b
Vadim <vadim192000@gmail.com>
2022-12-12 18:37:49 +0300

    initial frontend (setting configs also tailwind)

---
+++ frontend/app/components/layout/Sidebar/Sidebar.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+import styles from './Navigation.module.scss'
+
+const Sidebar: FC = () => {
+	return <div>Sidebar</div>
+}
+
+export default Sidebar
---
+++ frontend/pages/index.tsx
@@ -0,0 +1,8 @@
+import Home from '@/components/screens/Home/Home'
+import styles from '../styles/Home.module.scss'
+
+export default function HomePage() {
+  return (
+    <Home />
+  )
+}
