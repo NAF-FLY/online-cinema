d02a4bed165db11959db5f3db3e9f8a70415d38f
Vadim <vadim192000@gmail.com>
2022-12-12 20:24:43 +0300

    implemented menuContainer

---
+++ frontend/app/components/layout/Navigation/MenuContainer/Menu.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react'
+
+import styles from './Menu.module.scss'
+import MenuItem from './MenuItem'
+import { IMenu } from './menu.interface'
+import AuthItems from './auth/AuthItems'
+
+const Menu: FC<{ menu: IMenu }> = ({ menu: { items, title } }) => {
+	return (
+		<div className={styles.menu}>
+			<div className={styles.heading}>{title}</div>
+			<ul className={styles.ul}>
+				{items.map((item) => (
+					<MenuItem item={item} key={item.link} />
+				))}
+				{title === 'General' ? <AuthItems /> : null}
+			</ul>
+		</div>
+	)
+}
+export default Menu
