ac41f6013683b3c3724dc2ce66b20f1b8fe8ac70
Vadim <vadim192000@gmail.com>
2022-12-13 21:58:10 +0300

    implemented skeleton loader

--- frontend/app/components/layout/Navigation/MenuContainer/genres/GenreMenu.tsx
+++ frontend/app/components/layout/Navigation/MenuContainer/genres/GenreMenu.tsx
@@ -1,3 +1,5 @@
+import SkeletonLoader from '@/ui/SkeletonLoader'
+
 import Menu from '../Menu'
 
 import { usePopularGenres } from './usePopularGenres'
@@ -6,7 +8,9 @@ const GenreMenu = () => {
 	const { isLoading, data } = usePopularGenres()
 
 	return isLoading ? (
-		<div className="mx-11 mb-6">Loading...</div>
+		<div className="mx-11 mb-6">
+			<SkeletonLoader count={5} className="h-7 mt-6" />
+		</div>
 	) : (
 		<Menu
 			menu={{
